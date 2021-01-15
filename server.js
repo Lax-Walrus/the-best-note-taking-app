dc15a0ee9481a738b82dc71e0b61ad13d27c50d4
Kevin Lyons <zinckev@hotmail.com>
2021-01-14 17:14:07 -0800

    read and write file are functional bugs in not pulling up past notes and needs to refresh to view new notes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // express function and port
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 // data parsing , public folder connection and json writablility
 app.use(express.urlencoded({ extended: true }));
@@ -12,13 +13,61 @@ app.use(express.static("public"));
 
 // note data (DATA)
 const savedNotes = [];
+const savedTitle = [];
 
-// routes GET, PUT & DELETE
+// routes for html
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// routes for API
+
+// read past notes
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("no error");
+      const parseData = JSON.parse(data);
+      res.send(parseData);
+    }
+  });
+});
+
+//   add new notes
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("no error");
+      const objData = JSON.parse(data);
+      const noteBuild = [...objData, newNote];
+      const userNotes = JSON.stringify(noteBuild);
+
+      fs.writeFile(
+        path.join(__dirname, "db/db.json"),
+        userNotes,
+        (err, res) => {
+          if (err) throw err;
+        }
+      );
+    }
+  });
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("One could say I'm porting at " + "http://localhost:" + PORT);
 });
